ddc6cf5af92b08feb2fec704accf8c59a2a84621
MrNiz <76523353+mrniz@users.noreply.github.com>
2023-05-12 22:37:26 -0600

    Update older-newer-script.sh

--- assets/older-newer-script.sh
+++ assets/older-newer-script.sh
@@ -1,10 +1,19 @@
+clear
+echo script básico hecho por: MrNiz.
+echo tratando de actualizar haxe
+haxelib --global update haxelib
+clear
+
 echo script básico hecho por: MrNiz.
 echo removiendo lo innecesario
 haxelib remove newgrounds
-haxelib remove haxeflixel 
+haxelib remove flixel 
 haxelib remove flixel-addons
+clear
 
+echo script básico hecho por: MrNiz.
 echo instalando lo necesario.
 haxelib install newgrounds 1.1.5	
 haxelib install flixel 5.2.2
 haxelib install flixel-addons 3.0.2	
+clear
