693a4af088a03960aa9ea07ea33e40f0fac6c7e2
MrNiz <76523353+mrniz@users.noreply.github.com>
2023-05-12 23:07:16 -0600

    Update and rename install-all.bat to install-all-win.bat

--- assets/install-all-win.bat
+++ assets/install-all-win.bat
@@ -1,5 +1,5 @@
 cls
-echo last update: May 12 - 23:04 CDMX
+echo last update: May 12 - 23:01 CDMX - WIN VER: 1.0
 echo from haxelib
 haxelib install lime
 haxelib install openfl
@@ -10,9 +10,11 @@ haxelib install flixel-addons
 haxelib install hscript
 haxelib install polymod
 haxelib install actuate
+echo running... setups.
 haxelib run lime setup
 haxelib run lime setup flixel
 haxelib run flixel-tools setup
+echo downgrade some libs.
 haxelib install newgrounds 1.1.5	
 haxelib install flixel 5.2.2
 haxelib install flixel-addons 3.0.2	
@@ -24,4 +26,4 @@ haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
 haxelib git faxe https://github.com/uhrobots/faxe
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git extension-webm https://github.com/KadeDev/extension-webm
-haxelib run lime rebuild extension-webm windows
+haxelib run lime rebuild extension-webm linux
