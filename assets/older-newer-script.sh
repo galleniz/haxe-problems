9d8e27a4b1b5e3dfa2a9087687514c771b150f4c
MrNiz <76523353+mrniz@users.noreply.github.com>
2023-05-12 22:32:00 -0600

    Create older-newer-script.sh

---
+++ assets/older-newer-script.sh
@@ -0,0 +1,6 @@
+haxelib remove newgrounds
+haxelib install newgrounds 1.1.5	
+haxelib remove haxeflixel 
+haxelib install flixel 5.2.2
+haxelib remove flixel-addons
+haxelib install flixel-addons 3.0.2	
